06ee5f88149171cc675d6955bb88b5783d3885f0
AldoGutierrezD <aldogd24@gmail.com>
2023-11-09 17:31:56 -0600

    Nuevas imagenes y constancia.json

--- view/js/tramites-escolares.js
+++ view/js/tramites-escolares.js
@@ -3,7 +3,9 @@ var $ = jQuery;
 $(document).ready(function () {
 
     getPasosIMSS();
+    getPasosConstancias();
     getIMSS('Obtén tu número de seguro');
+    getConstancias('Realiza el pago');
 
 });
 
@@ -92,6 +94,94 @@ function getIMSS(paso) {
 }
 
 
+
+
+function getPasosConstancias() {
+
+    var html = "";
+    $("#constancia-pasos").empty();
+    $("#constancia-pasos").removeClass('transition_info');
+
+    $.getJSON('../../json/constancia.json', function (data) {
+
+        var array = data.constancia;
+        var list = $('#constancia-pasos');
+
+        var i = 1;
+
+        array.forEach(function (objeto) {
+
+            html += `
+                <button class='btn-becas-internas' data-tipo='constancia' data-paso='${objeto.titulo}'>
+                    <div class='btn-step'>${i}</div>
+                    <div>
+                        <h3>${objeto.titulo}</h3>
+                    </div>
+                </button>
+                `;
+            i++;
+
+        });
+
+        setTimeout(function () {
+            $('#constancia-pasos').addClass('transition_info');
+            list.append(html);
+        }, 200)
+
+    });
+
+
+}
+
+
+function getConstancias(paso) {
+
+    var html = "";
+    $("#constancia-list").empty();
+    $("#constancia-list").removeClass('transition_info');
+
+    $.getJSON('../../json/constancia.json', function (data) {
+
+        var array = data.constancia;
+        var list = $('#constancia-list');
+
+        array.forEach(function (objeto) {
+
+            if (objeto.titulo === paso) {
+
+                html += `
+                    <h4>${objeto.titulo}</h4>
+                    <ul>
+                `;
+
+                objeto.descripcion.forEach(function (desc) {
+
+                    html += `<li style='cursor: default;'>${desc}</li>`;
+
+                });
+
+                html += `</ul>`;
+
+                if (objeto.link != '') {
+                    html += `<a href='${objeto.link}' target='_blank' class='btn-primary-utl' style='margin-top: 15px;'>Enlace</a>`;
+                }
+            }
+
+
+        });
+
+        setTimeout(function () {
+            $('#constancia-list').addClass('transition_info');
+            list.append(html);
+        }, 200)
+
+    });
+
+}
+
+
+
+
 $(document).on('click', '.btn-becas-internas', function () {
     $('.btn-step').removeClass('active');
     $(this).find('.btn-step').addClass('active');
@@ -101,7 +191,7 @@ $(document).on('click', '.btn-becas-internas', function () {
 
     if (tipo == 'imss') {
         getIMSS(paso);
-    } else if (tipo == 'constancias') {
+    } else if (tipo == 'constancia') {
         getConstancias(paso);
     }
 });
