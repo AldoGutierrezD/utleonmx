288ee88a3a9517181ca2d6664342c04876939203
AldoGutierrezD <aldogd24@gmail.com>
2023-11-13 15:03:42 -0600

    Se subieron imágenes nuevas y calendarios

--- components/navbar.php
+++ components/navbar.php
@@ -16,8 +16,8 @@
                         <li><a href="https://aulavirtual.seg.guanajuato.gob.mx/materias/login/index.php" target="_blank"><img src="../../svg/aula-virtual.svg" alt="">Aula Virtual</a></li>
                         <li><a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1698001631&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26deeplink%3dowa%252f%26RpsCsrfState%3ddbd63f50-097d-4926-47b1-d027c06f7cee&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c" target="_blank"><img src="../../svg/outlook.svg" alt="">Outlook</a></li>
                         <li><a href="http://www.bibliotecaecest.mx/" target="_blank"><img src="../../svg/books.svg" alt="">Biblioteca</a></li>
-                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N__.jpg" target="_blank"><img src="../../svg/calendar.svg" alt="">2022 - 2023</a></li>
-                        <li><a href="https://utleon.edu.mx/resource/img/CALENDARIO%20LE%C3%93N.jpg" target="_blank"><img src="../../svg/calendar.svg" alt="">2023 - 2024</a></li>
+                        <li><a href="/src/img/CALENDARIO_LEON_22-23.webp" target="_blank"><img src="../../svg/calendar.svg" alt="">2022 - 2023</a></li>
+                        <li><a href="/src/img/CALENDARIO_LEON_23-24.webp" target="_blank"><img src="../../svg/calendar.svg" alt="">2023 - 2024</a></li>
                         <li><a href="https://www.utleon.edu.mx/public_html/transparencia/normateca/docs/normas/reglamento_academico.pdf" target="_blank"><img src="../../svg/rules.svg" alt="">Reglamento</a></li>
                     </ul>
                 </div>
